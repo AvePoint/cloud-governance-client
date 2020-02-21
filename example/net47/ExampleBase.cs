7f5f4b3ec30421d74833b03594209fa1dc62ee43
Sear Lin <xlin@avepoint.com>
2020-02-21 21:14:05 +0800

    add netframework 4.7 example

---
+++ example/net47/ExampleBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Example
+{
+    using Cloud.Governance.Client.Client;
+
+    public class ExampleBase
+    {
+        static ExampleBase()
+        {
+            Configuration.Default.BasePath = "https://api_base_url";
+            // Configure API key authorization: ClientId
+            Configuration.Default.AddApiKey("clientId", "ceb5...");
+
+            // Configure API key authorization: ClientSecret
+            Configuration.Default.AddApiKey("clientSecret", "BtH...");
+
+            // Configure API key authorization: UserPrincipalName
+            Configuration.Default.AddApiKey("userPrincipalName", "someone@example.com");
+        }
+    }
+}
---
+++ example/net47/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requestId = new LockSiteRequestExample().Submit();
+        }
+    }
+}
