290662ad674861c14f083a86d35568b409733e14
xlin <xin.lin@avepoint.com>
2021-03-29 17:04:24 +0800

    update header

--- example/csharp/netframework/TestData/Model/ChangeSiteAdminTestData.cs
+++ example/csharp/netframework/TestData/Model/ChangeSiteAdminTestData.cs
@@ -1,31 +1,4 @@
-﻿/********************************************************************
- *
- *  PROPRIETARY and CONFIDENTIAL
- *
- *  This file is licensed from, and is a trade secret of:
- *
- *                   AvePoint, Inc.
- *                   525 Washington Blvd, Suite 1400
- *                   Jersey City, NJ 07310
- *                   United States of America
- *                   Telephone: +1-201-793-1111
- *                   WWW: www.avepoint.com
- *
- *  Refer to your License Agreement for restrictions on use,
- *  duplication, or disclosure.
- *
- *  RESTRICTED RIGHTS LEGEND
- *
- *  Use, duplication, or disclosure by the Government is
- *  subject to restrictions as set forth in subdivision
- *  (c)(1)(ii) of the Rights in Technical Data and Computer
- *  Software clause at DFARS 252.227-7013 (Oct. 1988) and
- *  FAR 52.227-19 (C) (June 1987).
- *
- *  Copyright © 2021 AvePoint® Inc. All Rights Reserved. 
- *
- *  Unpublished - All rights reserved under the copyright laws of the United States.
- */
+﻿
 using System;
 using System.Collections.Generic;
 using System.Text;
